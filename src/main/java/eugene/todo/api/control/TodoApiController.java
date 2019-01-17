1a4ee50f59cca5311f2200682c32bd9934c229e9
Eugene Kim <eugene70kim@gmail.com>
2019-01-17 13:43:58 +0900

    불필요한 import 제거

--- src/main/java/eugene/todo/api/control/TodoApiController.java
+++ src/main/java/eugene/todo/api/control/TodoApiController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import eugene.todo.api.service.TodoApiService;
 import eugene.todo.vo.TodoVo;
